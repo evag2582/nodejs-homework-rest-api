28219962d4339c788feb02101b790ff71a346551
evag2582 <elio.g28@gmail.com>
2024-03-10 16:44:49 -0400

    update task2

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,61 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const res = contacts.find((item) => item.id === contactId.toString());
+  return res || null;
+};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const indexContact = contacts.findIndex(
+    (item) => item.id === contactId.toString()
+  );
+  if (indexContact === -1) return null;
+  const [res] = contacts.splice(indexContact, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return res;
+};
 
-const addContact = async (body) => {}
+const addContact = async (data) => {
+  const { name, email, phone } = data;
+  const contacts = await listContacts();
+  const newContact = { id: nanoid(), name, email, phone }; // Using nanoid here
 
-const updateContact = async (contactId, body) => {}
+  contacts.push(newContact);
+
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  try {
+    const contacts = await listContacts();
+    const indexContact = contacts.findIndex(
+      (item) => item.id === contactId.toString()
+    );
+
+    if (indexContact === -1) return null;
+
+    contacts[indexContact] = { ...contacts[indexContact], ...body };
+
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+    return contacts[indexContact];
+  } catch (error) {
+    console.error("Error updating contact:", error);
+    throw error; // Rethrow the error to be caught by the calling function
+  }
+};
+
 
 module.exports = {
   listContacts,
@@ -16,4 +63,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
